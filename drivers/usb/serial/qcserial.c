3d80faff0078d1adee2767113998dfb940c3c3fb
Bruce Bushby <bruce@core.home.local>
2015-06-05 14:39:19 +0100

    Added SIMCOM7100 to qcserial

--- drivers/usb/serial/qcserial.c
+++ drivers/usb/serial/qcserial.c
@@ -137,6 +137,7 @@ static const struct usb_device_id id_table[] = {
 	{USB_DEVICE(0x12D1, 0x14F0)},	/* Sony Gobi 3000 QDL */
 	{USB_DEVICE(0x12D1, 0x14F1)},	/* Sony Gobi 3000 Composite */
 	{USB_DEVICE(0x0AF0, 0x8120)},	/* Option GTM681W */
+	{USB_DEVICE(0x1e0e, 0x9001)},	/* SimCOM SIM7100E LTE*/
 
 	/* non-Gobi Sierra Wireless devices */
 	{DEVICE_SWI(0x0f3d, 0x68a2)},	/* Sierra Wireless MC7700 */
